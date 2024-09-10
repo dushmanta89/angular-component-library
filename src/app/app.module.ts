383c646336123c0db6975f81c98512d74de93287
dushmanta-tanty <dushmanta.tanty@gmail.com>
2024-09-11 00:06:34 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppMaterialModule } from './app-material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DatePickerComponent } from './components/date-picker/date-picker.component';
+import { PdsButtonComponent } from './components/pds-button/pds-button.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatePickerComponent,
+    PdsButtonComponent
   ],
   imports: [
     BrowserModule,
+    AppMaterialModule,
     AppRoutingModule
   ],
   providers: [],
